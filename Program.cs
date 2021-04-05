9b78f197245b31b3d6d88d8b927ae273a3eadc30
FabianoEtec <nbscbs@gmail.com>
2021-04-04 21:19:01 -0300

    A hora certa

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HoraSistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime horaSistema = DateTime.Now;
+            Console.WriteLine("═════ H O R A    D O    S I S T E M A ═════");
+            Console.WriteLine($"Conforme seu sistema a hora é : {horaSistema}");
+        }
+    }
+}
